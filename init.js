a6b5ff536f8178a4360aeb35c841494f30998b9a
rameshkrr <rameshkrr652@gmail.com>
2018-05-03 17:05:26 +0530

    first commit

---
+++ init.js
@@ -0,0 +1,30 @@
+﻿var init = { 
+     'questions': [ 
+       {
+           'question': 'jQuery is a...',
+           'answers': ['JavaScript library','Ruby Gem','PHP Framework','None of the above'],
+			  'correctAnswer': 1
+       },
+       {
+           'question': 'jQuery used in .. ',
+           'answers': ['Frontend','Backend'],
+			  'correctAnswer': 1
+       },
+		 {
+           'question': 'Javascript different from Java',
+           'answers': ['No, they two are same.','Yes, they are different programming languages'],
+			  'correctAnswer': 2
+       },
+     ],
+	  'resultComments' :  
+	  {
+		    perfect: 'Albus, is that you?',
+			 excellent: 'Outstanding, noble sir!',
+			 good: 'Exceeds expectations!',
+			 average: 'Acceptable. For a muggle.',
+			 bad: 'Well, that was poor.',
+			 poor: 'Dreadful!',
+			 worst: 'For shame, troll!'
+	  }
+
+ };
